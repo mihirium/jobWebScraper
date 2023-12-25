44fa6c2527591f0432947f22841a2623951c8c3b
Mihir Sangameswar <mihir.a.sangameswar@gmail.com>
2023-12-25 04:40:53 -0500

    added error checking and now returns a list

--- testWebScrape.py
+++ testWebScrape.py
@@ -8,13 +8,17 @@ def eligible(url):
     soup = BeautifulSoup(page.content, 'html.parser')
     qualifications_list = str(soup.find('div', {'id': 'jd-key-qualifications'}).find('ul', {'class': 'jd__list'}))
     experience_pattern = re.compile(r'(?:year|yr|yrs|years)')
-
+    jobDesc = []
     matches = experience_pattern.findall(qualifications_list)
     if len(matches) == 0:
-        return True
-    return False
+        title = soup.find('h1', {'class': 'jd__header--title'}).text
+        jobDesc.append(title)
+        jobDesc.append(url)
+    return jobDesc
 
 def getAppleJobs(sort, numPages=1):
+    appleJobList = []
+
     appleJobURL = "https://jobs.apple.com"
     
     for x in range(1, numPages + 1):
@@ -26,16 +30,21 @@ def getAppleJobs(sort, numPages=1):
         for element in elements_with_class:
             href_content = element.get("href")
             myUrl = appleJobURL + href_content
-            if eligible(myUrl):
-                print(myUrl)
+            # print("THIS IS THE URL ACCESSING", myUrl)
+            myPage = requests.get(myUrl)
+            if(myPage.status_code == 200):
+                jobData = eligible(myUrl)
+                if len(jobData) != 0:
+                    appleJobList.append(jobData)
+    return appleJobList
 
 def main():
     parser = argparse.ArgumentParser(description="Apple Jobs Scraper")
     parser.add_argument("--sort", default="relevance", help="Sorting type (default: relevance)")
-    parser.add_argument("--pages", type=int, default=1, help="Number of pages to scrape (default: 3)")
+    parser.add_argument("--pages", type=int, default=1, help="Number of pages to scrape (default: 1)")
     args = parser.parse_args()
 
-    getAppleJobs(args.sort, args.pages)
+    print(getAppleJobs(args.sort, args.pages))
 
 if __name__ == "__main__":
     main()
